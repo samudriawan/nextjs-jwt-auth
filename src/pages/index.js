5e72b23d37c97582905bba696b90fcf5c1e4a16a
Dyota <samudriawan@yahoo.co.id>
2022-09-29 12:35:43 +0700

    initial setup with jwt and sass

---
+++ src/pages/index.js
@@ -0,0 +1,16 @@
+import Head from 'next/head';
+
+export default function Home() {
+	return (
+		<div>
+			<Head>
+				<title>Nextjs JWT App</title>
+				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+				<meta name="description" content="next jwt authorization app" />
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+
+			<main>Initial Setup Template</main>
+		</div>
+	);
+}
