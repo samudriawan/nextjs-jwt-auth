c853069d3d42137c1a06a1892d87a5f6d3d759ea
Dyota <samudriawan@yahoo.co.id>
2022-11-24 12:40:03 +0700

    add json data file

--- src/pages/api/users/authenticate.js
+++ src/pages/api/users/authenticate.js
@@ -2,23 +2,7 @@
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import cookie from 'cookie';
-
-const LOCAL_DATA = [
-	{
-		email: 'example@example.com',
-		hash: '$2a$10$Z8qEQjShwKxM72UPrgReIOdLIZslhIDxcU0KQkjmCzSsrcxZ9PSd2', // password
-		dateCreated: '10/2/2022, 5:06:23 PM',
-		dateModified: '10/2/2022, 5:06:24 PM',
-		id: 1,
-	},
-	{
-		email: 'example2@example.com',
-		hash: '$2a$10$Z8qEQjShwKxM72UPrgReIOdLIZslhIDxcU0KQkjmCzSsrcxZ9PSd2', // password
-		dateCreated: '10/2/2022, 5:06:23 PM',
-		dateModified: '10/2/2022, 5:06:24 PM',
-		id: 2,
-	},
-];
+import users from '../../../data/db.json';
 
 export default async function handler(req, res) {
 	const { email, password } = req.body;
@@ -28,7 +12,8 @@ export default async function handler(req, res) {
 	if (method !== 'POST')
 		return res.status(500).json('Method is not supported.');
 
-	const user = LOCAL_DATA.find((x) => x.email === email);
+	// const user = LOCAL_DATA.find((x) => x.email === email);
+	const user = users.find((x) => x.email === email);
 
 	if (!user)
 		return res.status(400).json({ success: false, msg: 'User did not exist' });
@@ -51,26 +36,24 @@ export default async function handler(req, res) {
 	const refreshToken = jwt.sign(
 		{ email: user.email },
 		process.env.NEXT_PUBLIC_REFRESH_TOKEN_KEY,
-		{ expiresIn: '7d' }
+		{ expiresIn: '1h' }
 	);
 
 	res.setHeader(
 		'Set-Cookie',
 		cookie.serialize('jwt', refreshToken, {
 			httpOnly: true, // accessible only by web server
-			secure: true, // https
-			sameSite: 'None', // cross-site cookie
-			maxAge: 7 * 24 * 60 * 60 * 1000, // cookie expire, set to match refresh token
+			// secure: true, // https
+			// sameSite: 'None', // cross-site cookie
+			maxAge: 60 * 60 * 1000, // cookie expire, set to match refresh token
+			// maxAge: 7 * 24 * 60 * 60 * 1000, // cookie expire, set to match refresh token
+			path: '/',
 		})
 	);
 
 	return res.json({
 		success: true,
-		user: {
-			email: user.email,
-			id: user.id,
-			dateCreated: user.dateCreated,
-			accessToken,
-		},
+		email: user.email,
+		token: accessToken,
 	});
 }
