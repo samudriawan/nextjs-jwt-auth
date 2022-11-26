0c36533a2ef0b1cba9e9d719898c9ae664faca9f
Dyota <samudriawan@yahoo.co.id>
2022-11-26 18:28:59 +0700

    change retrieve user data from local json to mongoDB

--- src/pages/api/users/index.js
+++ src/pages/api/users/index.js
@@ -1,18 +1,21 @@
 import { apiHandler } from '../../../helpers/api/api-handler';
-import users from '../../../data/db.json';
+import User from '../../../model/User';
 
 export default apiHandler({
 	get: getUser,
 });
 
-function getUser(req, res) {
+async function getUser(req, res) {
 	// get email from token
 	const email = req.email;
 
-	const user = users.find((u) => u.email === email);
+	// get user data without hash and refresh_token
+	const foundUser = await User.findOne({ email: email }).select(
+		'_id email createdAt updatedAt'
+	);
 
 	// if email from token not match with in the database
-	if (!user) return res.status(401).send('credentials not match');
+	if (!foundUser) return res.status(401).send('credentials not match');
 
-	return res.status(200).json(user);
+	return res.status(200).json(foundUser);
 }
